1ead9a21f1becc32e3ce8313ac64006b548fe61f
xzyfer <xzyfer@gmail.com>
2015-05-18 15:09:51 +1000

    Initial implementation

---
+++ index.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+var escape = function (str) {
+    return str.replace(/[\[\]\/{}()*+?.\\^$|-]/g, '\\$&');
+};
+
+var regexp = ['lib', 'test'].map(function (i) {
+    return '^' + escape(path.join(__dirname, i) + path.sep);
+}).join('|');
+
+require('babel-core/register')({
+    only:   new RegExp('(' + regexp + ')'),
+    ignore: false,
+    loose: 'all'
+});
+module.exports = require('./lib/entry');
